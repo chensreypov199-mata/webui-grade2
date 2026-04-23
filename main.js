98092996876bc6101289137feb4e366115d67789
chensreypov199-mata <chensreypov199@gmail.com>
2026-04-23 17:13:09 +0900

    my firsrt commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// alert("Hello word")
+
+// const name = prompt("名前は？");
+
+// alert("こんにちは " + name + " さん！");
+
+const target = document.getElementById("target");
+target.textContent = "ばか"
+target.style.color = "red";
+target.style.fontSize = "yellow";
+target.style. background ="yellow";
+
+
+
+
+
+
+
