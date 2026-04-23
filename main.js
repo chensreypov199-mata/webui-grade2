d8a0a70242b0475e85b384c37a31ebe2fd69e5ad
chensreypov199-mata <chensreypov199@gmail.com>
2026-04-23 17:58:07 +0900

    counter funtion create

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-// alert("Hello word")
-
-// const name = prompt("名前は？");
-
-// alert("こんにちは " + name + " さん！");
-
-const target = document.getElementById("target");
-target.textContent = "ばか"
-target.style.color = "red";
-target.style.fontSize = "yellow";
-target.style. background ="yellow";
-
-
-
-
-
-
-
+function sayHello  () {
+    alert("Hello");
+}
